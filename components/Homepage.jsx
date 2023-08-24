1c2b5df6d8a4149c2a1de893a2bc8e0909602c2b
Jawad762 <mjawadzaiter@gmail.com>
2023-08-24 13:51:27 +0300

    first commit

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
   return (
     <main className='flex flex-row w-full justify-between mt-16 pb-16 max-md:flex-col max-md:pb-0 max-md:gap-8 max-sm:gap-2'>
         <div className='flex flex-col items-left w-1/2 relative left-16 gap-6 max-md:w-full max-lg:w-1/2 max-lg:left-4 max-lg:left-4'>
-            <h1 className='font-bold text-6xl w-1/2 leading-relaxed max-sm:w-1/2 max-md:w-full max-lg:text-4xl'>Find, book, rent a car—quick and super easy!</h1>
+            <h1 className='font-bold text-6xl w-1/2 leading-relaxed max-sm:w-2/3 max-md:w-full max-lg:text-4xl'>Find, book, rent a car—quick and super easy!</h1>
             <p className='text-3xl w-9/12'>Streamline your car rental experience with our effortless booking process.</p>
             <button onClick={handleExploreCars} className="bg-blue-600 text-white rounded-full py-3 px-6 w-fit mt-2 text-lg">Explore Cars</button>
         </div>
